d6fce9ae757cf1296fe63b88f9cca5db47395eb7
HyunseokLEE <unducklee@gmail.com>
2023-01-06 19:08:43 +0900

    feat: fetch

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,9 @@
+import Lee from "../components/Lee";
+
 export default function About() {
   return (
     <div>
+      <Lee title="about" />
       <h1>about</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-import NavBar from "../components/NavBar";
+import Lee from "../components/Lee";
+import { useState, useEffect } from "react";
+
+const API_KEYS = "cf5feb3192bfa94ddb5020cdf200f5fc";
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEYS}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
-      <h1 className="active">Hello</h1>
+      <Lee title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
